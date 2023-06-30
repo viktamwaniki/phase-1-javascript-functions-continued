dee40cfe54fc334d2b748a9da98af21bc7a6eb9d
delamoar <you@example.com>
2023-06-30 13:44:30 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+ function saturdayFun(day = "Saturday", activity = "roller-skate") {
+    console.log(`This ${day}, I want to  ${activity}!`);
+  }
+  saturdayFun(saturday, bathe-my-dog );
+
+
+  function mondayWork(day = "Monday", activity = "work from home") {
+    console.log(`This ${day}, I will  ${activity}.`);
+  }
+  mondayWork(Monday, go-to-the-office);
+ 
+
+
+function wrapAdjective(adjective){
+}
